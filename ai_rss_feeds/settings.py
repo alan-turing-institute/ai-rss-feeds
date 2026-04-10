b225170b1263262933aed8a0c5d4ec5c2e89c56f
Christopher Rowley <crowley@turing.ac.uk>
2026-04-10 12:24:22 +0100

    fix httpcache location

--- ai_rss_feeds/settings.py
+++ ai_rss_feeds/settings.py
@@ -5,7 +5,7 @@ NEWSPIDER_MODULE = "ai_rss_feeds.spiders"
 
 # Enable HTTP caching unconditionally; delete .scrapy/httpcache to refresh
 HTTPCACHE_ENABLED = True
-HTTPCACHE_DIR = ".scrapy/httpcache"
+HTTPCACHE_DIR = "httpcache"
 HTTPCACHE_EXPIRATION_SECS = 0  # Never expire automatically
 
 LOG_LEVEL = "INFO"
